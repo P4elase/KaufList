8467f3f63c5abc1a61ddc178c9c19bd238030d65
P4elase <164899551+P4elase@users.noreply.github.com>
2024-04-08 14:27:04 +0300

    fix

--- static/script.js
+++ static/script.js
@@ -101,7 +101,6 @@ function loadList() {
             }
             // Создаем новую кнопку удаления
             let deleteButton = document.createElement("button");
-            deleteButton.setAttribute('id="btn3"');
             deleteButton.textContent = "Удалить";
             deleteButton.addEventListener("dblclick", function () {
                 localStorage.removeItem(key);
@@ -117,7 +116,6 @@ function loadList() {
             pElement.setAttribute("data-key", key);
             pElement.innerHTML = `Продукт: ${key}, Количество: ${value}`;
             let deleteButton = document.createElement("button");
-            deleteButton.setAttribute('id="btn3"');
             deleteButton.textContent = "Удалить";
             deleteButton.addEventListener("dblclick", function () {
                 localStorage.removeItem(key);
